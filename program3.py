311efa4f43c442086201c37a55bfa5d5103d29ed
prpd1 <praveenapedapati@gmail.com>
2022-05-05 19:55:49 +0530

    created

---
+++ program3.py
@@ -0,0 +1,7 @@
+#Write a program to find the square and cube of a given number.
+
+x = int(input("enter a number:"))
+square = x**2
+cube = x**3
+print(f'square is {square}')
+print(f'cube is {cube}')
