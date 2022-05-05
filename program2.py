b1f615973623d37eb6543592cc2b244c1f5c06d0
prpd1 <praveenapedapati@gmail.com>
2022-05-05 19:51:38 +0530

    created

---
+++ program2.py
@@ -0,0 +1,8 @@
+#Write a program to multiply and divide two numbers.
+
+x = int(input("enter a number:"))
+y = int(input("enter a number:"))
+multiply = x*y
+divide = x/y
+print(f'multiplication is {multiply}')
+print(f'division is {divide}')
