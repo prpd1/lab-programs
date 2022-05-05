6895639e4d9ddd415ef082e001c3e2fe361529b3
prpd1 <praveenapedapati@gmail.com>
2022-05-05 19:42:40 +0530

    created

---
+++ program1.py
@@ -0,0 +1,8 @@
+#Write a program to add and subtract two numbers.
+
+x = int(input("enter a number:"))
+y = int(input("enter a number:"))
+add = x+y
+substract = x-y
+print(f'addition is {add}')
+print(f'substract is {substract}')
