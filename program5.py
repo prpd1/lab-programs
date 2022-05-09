d24ed82ad4de3541c468ee59d151fb008bae7712
prpd1 <praveenapedapati@gmail.com>
2022-05-09 14:13:20 +0530

    created

---
+++ program5.py
@@ -0,0 +1,7 @@
+#Write a program to find the area and perimeter of a square.
+
+x = int(input("enter a number:"))
+area = x**2
+perimeter = 4*x
+print(f'area of square is {area}')
+print(f'perimeter of sqaure is {perimeter}')
